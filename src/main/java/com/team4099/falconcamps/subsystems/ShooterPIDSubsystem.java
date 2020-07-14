f754dbcaa8c8cc5cc8faccea90d4a3eacabb4adb
kdbrill <brik8110@chsphantoms.org>
2020-07-14 18:32:32 +0000

    Day 1

--- src/main/java/com/team4099/falconcamps/subsystems/ShooterPIDSubsystem.java
+++ src/main/java/com/team4099/falconcamps/subsystems/ShooterPIDSubsystem.java
@@ -1,23 +1,35 @@
 package com.team4099.falconcamps.subsystems;
 
 import com.revrobotics.*;
+
+import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ShooterPIDSubsystem extends SubsystemBase {
+public class ShooterPIDSubsystem extends PIDSubsystem {
     private final CANSparkMax shooterLeader = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
     private final CANSparkMax shooterFollower = new CANSparkMax(14, CANSparkMaxLowLevel.MotorType.kBrushless);
     private final CANEncoder shooterEncoder = shooterLeader.getEncoder();
 
     public ShooterPIDSubsystem() {
-        double shooterLeader = 1.7 / 300;
-        double shooterFollower = 0.0;
-        double shooterEncoder = 5.5 * 14000
-        super (new PIDController(shooterLeader , shooterFollower , shooterEncoder));   
-    }
-    public double useMeasurement() {
-        return getCurrentCommand();
+        double shooterP = 1.7 / 300;
+        double shooterI = 0.0;
+        double shooterD = 5.5 * 14000;
+        super (new PIDController(shooterP, shooterI, shooterD));
+        shooterFollower.follow (shooterLeader, true);
     }
 
     @Override
     public void periodic() {}
+
+    @Override
+    protected void useOutput(double output, double setpoint) {
+shooterLeader.set (output);
+
+    }
+
+    @Override
+    protected double getMeasurement() {
+        
+        return shooterEncoder.getVelocity();
+    }
 }
